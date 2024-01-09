ddb8abda10bde1c6605d7137593f079fc3767e50
Itakello <maxste000@gmail.com>
2024-01-09 21:20:07 +0100

    Removed API key

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from utilities import *
 
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
-config_list = autogen.config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+config_list = autogen.config_list_from_json(
+    env_or_file = "OAI_CONFIG_LIST",
+    filter_dict= {"model":"gpt-3.5-turbo-1106"}
+    )
 llm_config = {"config_list": config_list}
 
 prisoner = create_conversable_agent("prisoner", llm_config, get_prisoner_SM())
@@ -34,12 +37,12 @@ manager = autogen.GroupChatManager(
 researcher.initiate_chat(
     recipient = manager,
     clear_history=True,
-    message = "Initiate the simulation!!!"
+    message = "Initiate the simulation"
     )
 
 #guard.human_input_mode = "ALWAYS"
 
-"""researcher.initiate_chat(
+researcher.initiate_chat(
     recipient=guard,
     message="Make a summary about the conversation you just had with the prisoner."
-)"""
+)
